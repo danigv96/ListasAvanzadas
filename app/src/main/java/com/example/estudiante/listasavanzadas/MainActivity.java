389619f57a37d36ee6ff8033814180f7e7a5067d
estudiante <ad>
2018-08-30 13:03:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estudiante/listasavanzadas/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.estudiante.listasavanzadas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lv_noticias;
+    NoticiaAdapter customAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv_noticias = findViewById(R.id.lv_noticias);
+        customAdapter = new NoticiaAdapter(this);
+        lv_noticias.setAdapter(customAdapter);
+
+        Noticia noticia1 = new Noticia("Logo nuevo de HED", "Este año el logo de HED tendra un cambio", "30/08/2018");
+
+        customAdapter.agregarNoticia(noticia1);
+
+        lv_noticias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Noticia noticia_click = (Noticia) customAdapter.getItem(position);
+                Toast.makeText(MainActivity.this, noticia_click.getTitulo(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/estudiante/listasavanzadas/NoticiaAdapter.java
@@ -0,0 +1,62 @@
+package com.example.estudiante.listasavanzadas;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NoticiaAdapter extends BaseAdapter {
+
+    ArrayList<Noticia> noticias;
+    Activity activity;
+
+    public NoticiaAdapter (Activity activity){
+        this.activity = activity;
+        noticias = new ArrayList<>();
+    }
+
+
+    @Override
+    public int getCount() {
+        return noticias.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return noticias.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    //generar un renglon por objeto
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+
+        //transformar un xml "txt" en una vista "view", para retornalo (objeto que se puede controlar)
+        LayoutInflater inflater = activity.getLayoutInflater();
+
+        View renglon = inflater.inflate(R.layout.renglon, null, false);
+
+        TextView item_titulo=renglon.findViewById(R.id.item_titulo);
+        TextView item_fecha = renglon.findViewById(R.id.item_fecha);
+        TextView item_description = renglon.findViewById(R.id.item_descripcion);
+
+        item_titulo.setText(noticias.get(position).getTitulo());
+        item_fecha.setText(noticias.get(position).getFecha());
+        item_description.setText(noticias.get(position).getDescripcion());
+
+        return renglon;
+    }
+
+    public void agregarNoticia(Noticia noticia){
+        noticias.add(noticia);
+        notifyDataSetChanged();
+    }
+}
