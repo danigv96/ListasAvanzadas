940ddc47e94801a92b0d3a4c6926d389a18f21f0
estudiante <ad>
2018-08-31 15:25:57 -0500

    CODIGO AVANZADO TERCERA EDICION

--- app/src/main/java/com/example/estudiante/listasavanzadas/MainActivity.java
+++ app/src/main/java/com/example/estudiante/listasavanzadas/MainActivity.java
@@ -4,18 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView lv_noticias;
     NoticiaAdapter customAdapter;
+    EditText et_titulo;
+    Button btn_generar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_titulo = findViewById(R.id.et_titulo);
+        btn_generar = findViewById(R.id.btn_generar);
 
         lv_noticias = findViewById(R.id.lv_noticias);
         customAdapter = new NoticiaAdapter(this);
@@ -32,6 +41,28 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 Noticia noticia_click = (Noticia) customAdapter.getItem(position);
                 Toast.makeText(MainActivity.this, noticia_click.getTitulo(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn_generar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //Traernos la fecha actual
+                Calendar c = Calendar.getInstance();
+                int year =c.get(Calendar.YEAR);
+                int month =c.get(Calendar.MONTH);
+                int day =c.get(Calendar.DAY_OF_MONTH);
+                month++;
+                String fecha = day+"/"+month+"/"+year;
+
+
+
+                String titulo= et_titulo.getText().toString();
+                //Noticia newNoticia = new Noticia(titulo, "NO DESCRIPTION", "00/00/00");
+                Noticia newNoticia = new Noticia(titulo, "NO DESCRIPTION", fecha);
+
+                customAdapter.agregarNoticia(newNoticia);
             }
         });
     }
